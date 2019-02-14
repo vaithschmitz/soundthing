992e34438a221efc629a9b54194e33f9c56362e7
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-02-14 23:09:37 +0000

    testbuild for troubleshooting

--- src/App.js
+++ src/App.js
@@ -36,45 +36,62 @@ class App extends Component {
   }
  
   hitDrum = (type) =>{
-    const synth = new Tone.PolySynth(4, Tone.Synth);
-    synth.toMaster();
+  //   const synth = new Tone.PolySynth(4, Tone.Synth);
+  //   synth.toMaster();
  
-    const notes = [
-      // Chord.notes("C2add9"), Chord.notes("D2m7"), Chord.notes("C2maj7"),
-      Chord.notes("C3add9"), Chord.notes("D3m7"), Chord.notes('E3m7'), Chord.notes("C3maj7"),
-      // Chord.notes("C4add9"), Chord.notes("D4m7"), Chord.notes("C4maj7"),
-    ]
+  //   const notes = [
+  //     // Chord.notes("C2add9"), Chord.notes("D2m7"), Chord.notes("C2maj7"),
+  //     Chord.notes("C3add9"), Chord.notes("D3m7"), Chord.notes('E3m7'), Chord.notes("C3maj7"),
+  //     // Chord.notes("C4add9"), Chord.notes("D4m7"), Chord.notes("C4maj7"),
+  //   ]
     
-    function loop(){
-      let loopIndex = 0;
-      Tone.Transport.scheduleRepeat(time => {
-      repeat(time)
-    }, '16n');
+  //   function loop(){
+  //     let loopIndex = 0;
+  //     Tone.Transport.scheduleRepeat(time => {
+  //     repeat(time)
+  //   }, '16n');
 
-    function repeat(time){
-      let note = notes[loopIndex % notes.length];
-      synth.triggerAttackRelease(note, '16n', time);
-      loopIndex++ ;
-      console.log(loopIndex);
-    }
+  //   function repeat(time){
+  //     let note = notes[loopIndex % notes.length];
+  //     synth.triggerAttackRelease(note, '16n', time);
+  //     loopIndex++ ;
+  //     console.log(loopIndex);
+  //   }
 
-    Tone.Transport.start();
-    setTimeout(() => {
-      Tone.Transport.stop();
-    }, 5000)
-  }
+  //   Tone.Transport.start();
+  //   setTimeout(() => {
+  //     Tone.Transport.stop();
+  //   }, 5000)
+  // }
+    const sampler = new Tone.Sampler({
+      'C3' : '../sounds/Kick.wav', 
+      'D3' : '../sounds/Snare.wav',
+      'E3' : '../sounds/Clap.wav',
+      'F3' : '../sounds/HH.wav',
+      'G3' : '../sounds/Ride.wav'
+    }, {
+      'onload': function() { 
+        console.log('loaded'); 
+      }});
+  
+    sampler.toMaster();
+  
     
     switch (type){
       case 'Arp':
-        loop();
+        sampler.triggerAttack('C3');
         break;
         case 'A':
+        sampler.triggerAttack('D3');
         break;
         case 'B':
+        sampler.triggerAttack('E3');
         break;
         case 'C':
+        sampler.triggerAttack('F3');
         break;
         case 'D':
+        sampler.triggerAttack('G3');
         break;
         case 'E':
         break;
