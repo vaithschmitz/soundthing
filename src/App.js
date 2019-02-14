5e62b858cf3bc2ca43d986813d413e6ce2a52561
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-02-14 20:54:32 +0000

    added tonejs loop

--- src/App.js
+++ src/App.js
@@ -1,28 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Drums from "./components/Drums.js";
+import Tone from 'tone';
+import {Chord, PcSet} from 'tonal';
+
 
 class App extends Component {
+  state={
+   drums: [
+      {
+        id: 1, 
+        type: "Kick"
+      },
+      {
+        id: 2, 
+        type: "Snare"
+      },
+      {
+        id: 3, 
+        type: "Clap"
+      },
+      {
+        id: 3, 
+        type: "HH"
+      },
+      {
+        id: 4, 
+        type: "Ride"
+      },
+      {
+        id: 4, 
+        type: "Play"
+      }
+    ]
+  }
+ 
+  hitDrum = (type) =>{
+    const synth = new Tone.PolySynth(4, Tone.Synth);
+    synth.toMaster();
+ 
+    const notes = [
+      Chord.notes("C3add9"), Chord.notes("D3m7"), Chord.notes("C3maj7")
+    ]
+    
+    function loop(){
+      let loopIndex = 0;
+      Tone.Transport.scheduleRepeat(time => {
+      repeat(time)
+    }, '16n');
+
+    function repeat(time){
+      let note = notes[loopIndex % notes.length];
+      synth.triggerAttackRelease(note, '16n', time);
+      loopIndex++ ;
+      console.log(loopIndex);
+    }
+
+    Tone.Transport.start();
+    setTimeout(() => {
+      Tone.Transport.stop();
+    }, 5000)
+  }
+    
+    switch (type){
+      case 'Kick':
+        loop();
+        break;
+        case 'Snare':
+        break;
+        case 'Clap':
+        break;
+        case 'HH':
+        alert("HH")
+        break;
+        case 'Ride':
+        alert("Ride")
+        break;
+        case 'Play':
+        alert("Play")
+        break;
+
+
+    }
+
+  }
+
+
   render() {
-    return (
+    const cmaj7 = Chord.notes("CMaj7")
+    console.log(cmaj7)
+    return ( 
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Drums drums={this.state.drums} hitDrum={this.hitDrum}/>
       </div>
     );
   }
 }
 
+
 export default App;
+
---
+++ src/components/Perc.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Perc extends Component {
+
+    drumStyle= ()=>{
+        return{
+            // #394053 color scheme for elements left to right
+            // #4E4A59
+            // #6E6362
+            // #839073
+            // #7CAE7A
+            background: 'lightgrey',
+            margin: '10px',
+            padding: '10px',
+            borderBottom: '1px, #ccc dotted', 
+            textAlign: 'center'
+
+        }
+    }
+  render() {
+      const { id, type } = this.props.drum;
+      return (
+    <div style={this.drumStyle()} onClick={this.props.hitDrum.bind
+    (this, type)} >
+    <h3>
+        {this.props.drum.type} 
+    </h3>
+    </div>
+      );
+  }
+}
+
+// PropTypes
+Perc.propTypes = {
+    drum: PropTypes.object.isRequired
+    }
+
+export default Perc
