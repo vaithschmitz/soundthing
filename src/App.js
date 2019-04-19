8c6be1db5c290c936fabbbf1ec7ad1f02a0e11ac
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-19 18:03:14 +0100

    iterative tone generation

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Drumpad from './components/Drumpad.js'
 class App extends Component {
   render() {
 
-    const pads = []
+    const pads = [];
+    let notes = ['C2','D2','E2','F2','G2','A2','B2','C3','D3','E3','F3','G3','A3','B3','C4','D4'];
     for(let i = 0; i< 16; i++){
-      pads.push(<Drumpad/>)
+      pads.push(<Drumpad note = {notes[i]}/>)
     }
 
     return ( 
       <div className="App"> 
+
         {pads}
       </div>
     );
--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,33 +1,26 @@
 import React, {Component} from 'react';
 import Tone from 'tone';
+import './Drumpad.css'
 
 class Drumpad extends Component {
   constructor(props){
     super(props)
-
     this.playSound = this.playSound.bind(this);
   }
 
-  playSound(){
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease("C2", '8n')
+  playSound(props){
+    const synth = new Tone.Synth().toMaster();
+      synth.triggerAttackRelease(this.props.note, '8n')
   }
-
+  
   render() {
     return (
-      <div 
-        className="pad" 
-        onClick={(e) => {this.playSound()}}
-        style={{
-          backgroundColor: 'blue',
-          width: '15vw',
-          color: 'white',
-          textAlign: 'center',
-          borderRadius: '10%',
-        }}
-        >
-        CLICK
+      <div
+        className='pad' 
+        onClick={(e) => {this.playSound()}}>
+        <h1> {this.props.note} </h1>
       </div>
+     
     );
   }
 } 
